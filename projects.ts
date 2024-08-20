14369b93f95c14c22eeb9c49b89c0fcc48b3e46a
Michal Valo <michal.valo6@gmail.com>
2024-08-20 18:45:56 +0200

    Add projects & move all types to one file

---
+++ projects.ts
@@ -0,0 +1,55 @@
+import type { Project } from './types';
+
+const projects: Project[] = [
+    {
+        name: 'Nivy',
+        slug: 'nivy',
+        role: 'Development',
+        url: 'https://nivy.com',
+        backgroundColor: '#cec5c3',
+    },
+    {
+        name: 'Volitaservis',
+        slug: 'volitaservis',
+        role: 'Development & Design',
+        url: 'https://volitaservis.sk',
+        backgroundColor: '#d4d9de',
+    },
+    {
+        name: 'Racetime',
+        slug: 'racetime',
+        role: 'Development & Design',
+        url: 'https://racetime.sk',
+        backgroundColor: '#bf5164',
+    },
+    {
+        name: 'STUDIO PARADA',
+        slug: 'studio-parada',
+        role: 'Development',
+        url: 'https://studioparada.com',
+        backgroundColor: '#1d1d1d',
+    },
+    {
+        name: 'ŠARMDENT',
+        slug: 'sarmdent',
+        role: 'Development',
+        url: 'https://sarmdent.sk',
+        backgroundColor: '#e8dde8',
+    },
+    {
+        name: 'TESTAR',
+        slug: 'testar',
+        role: 'Development & Design',
+        url: 'https://testar.sk',
+        backgroundColor: '#303c4b',
+    },
+    {
+        name: 'Excenter',
+        slug: 'excenter',
+        role: 'Development',
+        url: 'https://excenter.sk',
+        backgroundColor: '#3b4f60',
+    },
+];
+
+export default projects;
---
+++ types.ts
@@ -0,0 +1,19 @@
+export type Project = {
+    name: string;
+    slug: string;
+    role: string;
+    url: string;
+    backgroundColor: string;
+};
+
+export type Song = {
+    isPlaying: boolean;
+    name: string;
+    artistName: string;
+    songURL: string;
+};
+
+export type Social = {
+    name: string;
+    url: string;
+};
