2691f48b4a8a7632b65aaf96ae2a0cf953d9b9e1
Michal Valo <michal.valo6@gmail.com>
2024-10-29 21:56:06 +0100

    refactor: change imported path aliases to @

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import path from 'path';
 import icon from 'astro-icon';
 
 export default defineConfig({
+    vite: {
+        resolve: {
+            alias: {
+                '@': path.resolve('./src'),
+            },
+        },
+    },
     integrations: [
         tailwind(),
         icon({
