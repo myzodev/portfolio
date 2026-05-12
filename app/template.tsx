f2bc9abef1af2b38bb1ed508f802d1508346e8e1
Michal Valo <michal.valo6@gmail.com>
2026-05-12 02:04:59 +0200

    refactor: remove searchParams from template

--- app/template.tsx
+++ app/template.tsx
@@ -2,20 +2,18 @@
 
 import { useEffect } from "react";
 
-import { useSearchParams } from "next/navigation";
 import { usePathname } from "next/navigation";
 
 import { ReactLenis, useLenis } from "lenis/react";
 
 export default function Template({ children }: { children: React.ReactNode }) {
 	const pathname = usePathname();
-	const searchParams = useSearchParams();
 
 	const lenis = useLenis();
 
 	useEffect(() => {
 		lenis?.scrollTo(0, { immediate: true });
-	}, [pathname, searchParams, lenis]);
+	}, [pathname, lenis]);
 
 	return <ReactLenis root>{children}</ReactLenis>;
 }
