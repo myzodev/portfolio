d81d277b14ee4ff46660d66a32b1c311d7202a17
Michal Valo <michal.valo6@gmail.com>
2026-08-13 09:59:38 +0200

    feat: add hover to projects list item

--- app/projects/components/ProjectsListItem.tsx
+++ app/projects/components/ProjectsListItem.tsx
@@ -1,3 +1,6 @@
+import clsx from "clsx";
+import { ArrowUpRightIcon } from "lucide-react";
+
 import { PROJECT_PALETTES } from "@/constants/project-palettes";
 
 import type Project from "@/types/project";
@@ -19,9 +22,12 @@ export default function ProjectsListItem({ project, className = "" }: Props) {
 						"--project-bg-hover": palette.bgText,
 					} as React.CSSProperties
 				}
-				className="flex aspect-square items-center justify-center rounded-3xl bg-(--project-bg) p-5 transition-colors duration-500 group-hover:bg-(--project-bg-hover) md:rounded-4xl md:p-10"
+				className={clsx(
+					"relative flex aspect-square items-center justify-center rounded-3xl bg-(--project-bg) p-5 transition-colors duration-500 md:rounded-4xl md:p-10",
+					!project.wip && "group/item group-hover:bg-(--project-bg-hover)",
+				)}
 			>
-				<figure className="aspect-video w-full overflow-hidden rounded-lg md:rounded-xl">
+				<figure className="aspect-video w-full overflow-hidden rounded-lg transition-all duration-500 group-hover/item:scale-98 group-hover/item:opacity-30 md:rounded-xl">
 					{project.image ? (
 						<img
 							className="project-image h-[105%] w-full object-cover object-top"
@@ -37,12 +43,23 @@ export default function ProjectsListItem({ project, className = "" }: Props) {
 						</span>
 					)}
 				</figure>
+
+				{!project.wip && (
+					<span
+						className="absolute inset-0 z-10 flex items-center justify-center opacity-0 transition-opacity duration-500 group-hover/item:opacity-100"
+						aria-hidden
+					>
+						<span className="bg-peach flex size-16 items-center justify-center rounded-full transition-transform duration-500 group-hover/item:scale-110">
+							<ArrowUpRightIcon className="size-6" />
+						</span>
+					</span>
+				)}
 			</div>
 
-			<div className="border-ink-black-dimmed/25 flex flex-wrap items-baseline items-center justify-between gap-x-4 gap-y-1 border-b px-2 py-6 md:px-4 md:py-8">
+			<div className="border-ink-black-dimmed/25 flex flex-wrap items-center justify-between gap-x-4 gap-y-1 border-b px-2 py-6 md:px-4 md:py-8">
 				<h3
 					style={{ "--project-accent": palette.accentColor } as React.CSSProperties}
-					className="display-sm md:display-md lg:display-lg transition-colors duration-500 group-hover:text-(--project-accent)"
+					className={`display-sm md:display-md lg:display-lg transition-colors duration-500 ${!project.wip && "group-hover:text-(--project-accent)"}`}
 				>
 					{project.title}
 				</h3>
