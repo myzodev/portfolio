b4de031c54d7bb2b8b4407bae876968468242c9b
Michal Valo <michal.valo6@gmail.com>
2026-08-16 17:36:20 +0200

    fix: update LinkedIn URL in social links data

--- data/social-links.ts
+++ data/social-links.ts
@@ -2,7 +2,7 @@ import type Link from "@/types/link";
 
 const socialLinksData: Link[] = [
 	{ label: "Github", href: "https://github.com/myzodev" },
-	{ label: "LinkedIn", href: "https://www.linkedin.com/in/michal-valo-421762237/" },
+	{ label: "LinkedIn", href: "https://www.linkedin.com/in/michal-valo" },
 ];
 
 export default socialLinksData;
